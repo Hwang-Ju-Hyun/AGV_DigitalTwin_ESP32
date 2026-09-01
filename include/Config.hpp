002eccd3242a18e75baac093396d72d335825057
woojun0037 <67630480+woojun0037@users.noreply.github.com>
2026-09-01 16:21:14 +0900

    ip changed

--- include/Config.hpp
+++ include/Config.hpp
@@ -89,7 +89,7 @@ namespace AppConfig
     static constexpr const char* kWifiPassword = LocalSecrets::kWifiPassword;
 
     // Recheck this with ipconfig if the PC reconnects to Wi-Fi.
-    static constexpr char kServerHost[] = "192.168.45.226";
+    static constexpr char kServerHost[] = "192.168.45.102";
     static constexpr uint16_t kServerPort = 16666;
     static constexpr uint32_t kRequestedAgvID = 1;
 
